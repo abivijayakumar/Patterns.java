5cbd9f5e852bb364b0a7f8d7142a987609216b72
Abinaya <abivijayakumar419@gmail.com>
2025-04-22 08:12:10 +0530

    Create pyramid star pattern.java

---
+++ pyramid star pattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[]args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the input:");
+    int n = scan.nextInt();
+    */
+      i = 1, stars = 5, n = 5,stars= n-i+1
+      i = 2, stars = 4, n = 5
+      i = 3, stars = 3, n = 5
+      i = 4, stars = 2, n = 5
+      i = 5, stars = 1, n = 5
+      /*
+      for(int i=1;n<=n-i+1;i++){
+      for(int j=1;j<=n;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
