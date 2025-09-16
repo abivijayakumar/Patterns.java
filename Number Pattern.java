b6ad2bd591880aea72250c0b3611cc411e61ca21
Abinaya <abivijayakumar419@gmail.com>
2025-09-16 12:32:56 +0530

    Create Number Pattern.java

---
+++ Number Pattern.java
@@ -0,0 +1,23 @@
+import java.util*;
+public class Main{
+  public static void main(String[] args){
+    Sacnner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    for(int i=1;i<=(n*2)-1;i+=2)
+      {
+        for(int j=1;j<=i;j++)
+          {
+            System.out.print(j);
+          }
+        System.out.println();
+      }
+    for(int i=(n*2)-3;i>=1;i-=2)
+      {
+        for(int j=1;j<i;j++)
+          {
+            System.out.print(j);
+          }
+        if(i!=1)System.out.println();
+      }
+  }
+}
